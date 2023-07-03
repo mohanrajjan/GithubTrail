2aeb654be88cf270f228174fe93388848cbd2a7a
mohanrajjan <mohanrajjan@gmail.com>
2023-07-03 19:34:32 +0530

    first code

---
+++ src/test/java/org/gitubtrail/MYFirst.java
@@ -0,0 +1,12 @@
+package org.gitubtrail;
+
+public class MYFirst {
+	public void check(){
+		System.out.println("code1");
+		System.out.println("code2");
+	}
+public static void main(String[] args) {
+	MYFirst jj = new MYFirst();
+	jj.check();
+}
+}
